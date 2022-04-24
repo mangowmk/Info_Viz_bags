58384c4139306a6fb8fab6b58c9f0999389499c9
Jonathanlyj <jonathanlyj@163.com>
2022-04-24 19:09:45 -0400

    add bagPoint animation

--- BagPoint.js
+++ BagPoint.js
@@ -1,8 +1,5 @@
 
-
-
-
-function BagPoint(idx, startX, startY, brd, nm, stl, prc, bag_cl, sk_tp,vlm) {
+function BagPoint(idx, startX, startY, brd, nm, stl, prc, bag_cl, sk_tp, vlm) {
   var index = idx;
   var name = nm;
   var brand = brd;
@@ -11,6 +8,9 @@ function BagPoint(idx, startX, startY, brd, nm, stl, prc, bag_cl, sk_tp,vlm) {
   var bagColor = bag_cl;
   //var iconColor = cl;
   var skinType = sk_tp;
+  var volumn = vlm;
+  var nearestDot = false;
+ 
   
   // set these null so that they can be set the first time around
   var x = null;
@@ -18,7 +18,7 @@ function BagPoint(idx, startX, startY, brd, nm, stl, prc, bag_cl, sk_tp,vlm) {
 
 
   // this handles updating any animated variables
-  this.updatePos = function(){
+  this.updatePosition = function(){
     x.update();
     y.update();
   };
@@ -26,50 +26,89 @@ function BagPoint(idx, startX, startY, brd, nm, stl, prc, bag_cl, sk_tp,vlm) {
   this.displayDot = function(){
     // color the dots in gray, with no outline
     //fill(this.iconColor);
-    fill(bagPointColor);
     push();
     fill('#fae');
     strokeWeight(3);
     stroke(51);
-    if (this.style == "Crossbody Bag"){
-      ellipse(x.value, y.value, 13, 13);
+    if (style == "Crossbody Bag"){
+      ellipse(x.value + 6, y.value + 6, 13, 13);
     }
-    else if(this.style == "Shoulder Bag"){
-      rect(x.value, y.value, 20, 30);
+    else if(style == "Shoulder Bag"){
+      rect(x.value, y.value, 10, 15);
     }
-    else if(this.style == "Handbag"){
-      rect(x.value, y.value, 25, 25);
+    else if(style == "Handbag"){
+      rect(x.value, y.value, 13, 13);
     }
-    else if(this.style == "Crossbody Bag, Shoulder Bag"){
-      rect(x.value, y.value, 20, 30, 5);
+    else if(style == "Crossbody Bag, Shoulder Bag"){
+      rect(x.value, y.value, 10, 15, 2);
     }
-    else if(this.style == "Shoulder Bag, Handbag"){
-      ellipse(x.value, y.value, 12, 16);
+    else if(style == "Shoulder Bag, Handbag"){
+      ellipse(x.value + 6, y.value + 6, 12, 16);
     }
-    else if(this.style == "Crossbody Bag, Handbag"){
-      rect(x.value, y.value, 25, 25, 5);
+    else if(style == "Crossbody Bag, Handbag"){
+      rect(x.value, y.value, 13, 13, 3);
     }
-    else if(this.style == "Crossbody Bag, Shoulder Bag, Handbag"){
-      rect(x.value, y.value, 25, 25, 7);
+    else if(style == "Crossbody Bag, Shoulder Bag, Handbag"){
+      rect(x.value, y.value, 13, 13, 6);
     }
+  //  else{
+  //  console.log('unmatched!!!');
+  //}
     pop();
+    
   };
   
-  this.mouseHover = function(){
-    if (dist(mouseX, mouseY, x.value, y.value) < dotMouseRadius){
+  this.setNearest = function(nearest){
+    nearestDot = nearest;
+  };
+  
+  this.getDistance = function(){
+    return dist(mouseX, mouseY, x.value, y.value);
+  };
+  
+  this.getIndex = function(){
+    return index;
+  };
+ 
+  this.display = function(){
+    this.displayDot();
+    
+    
+    if (this.mouseHover() && nearestDot){
+      cursor(HAND);
       this.displayBagInfo();
       this.displayBagImg();
     }
+
+    
+  };
+  
+  this.mouseHover = function(){
+    var mouseDistance = dist(mouseX, mouseY, x.value, y.value);
+    if (mouseDistance <= dotMouseRadius){
+      return true;
+    }
+    else{
+    return false;
+  }
   };
   
   this.displayBagInfo = function(){
       push();
-      textAlign(CENTER);
+      textAlign(LEFT);
       // draw the value of this data point (using 1 decimal point)
       textSize(bagNameSize);
       fill(bagNameColor);
-      text(this.name, bagNameX, bagNameY);
+      text(name, bagNameX, bagNameY);
+      text(brand, bagNameX, bagNameY + bagNameSize);
       pop();
+  };
+  
+  this.displayBagImg = function(){
+  //image(bagImgs[name], bagInfoImgX, bagInfoImgY, bagInfoImgWidth, bagInfoImgWidth, 0, 0, bagSourceImgWidth, bagSourceImgHeight); 
+  var bagInfoImgWidth = bagImgs[name].width * 0.3;
+  var bagInfoImgHeight = bagImgs[name].height * 0.3;
+  image(bagImgs[name], bagImgLeftBottomX, bagImgLeftBottomY - bagInfoImgHeight, bagInfoImgWidth, bagInfoImgHeight);
   };
   
   // a function to set the 'index' (where it is in the array) 
@@ -78,7 +117,7 @@ function BagPoint(idx, startX, startY, brd, nm, stl, prc, bag_cl, sk_tp,vlm) {
     // use setTarget() instead of x= so that it will animate
     // if this is the first time it's being set, create the SoftNum
     if (x == null) {
-      x = new SoftNum(newX);
+      x = new SoftNum(newX, 0.5, 0.1);
     } else {
       x.setTarget(newX);
     }
@@ -88,11 +127,18 @@ function BagPoint(idx, startX, startY, brd, nm, stl, prc, bag_cl, sk_tp,vlm) {
     // use setTarget() instead of x= so that it will animate
     // if this is the first time it's being set, create the SoftNum
     if (y == null) {
-      y = new SoftNum(newY);
+      y = new SoftNum(newY, 0.5, 0.1);
     } else {
      y.setTarget(newY);
     }
   };
+ this.getPrice = function(){
+  return price;
+};
+ this.getVolumn = function(){
+  return volumn;
+};
+  
   // this sets the actual value for this data point
   
   // function to get the data point's value so it can be sorted
@@ -100,6 +146,6 @@ function BagPoint(idx, startX, startY, brd, nm, stl, prc, bag_cl, sk_tp,vlm) {
   // because these are inside DataPoint, not inside another function,
   // this code will run when "new DataPoint(idx, amt)" is called, 
   // setting the initial index and amount to the numbers passed in. 
-  this.setX(x);
-  this.setY(y);
+  this.setX(startX);
+  this.setY(startY);
 }
--- Bags.js
+++ Bags.js
@@ -5,35 +5,109 @@ var canvasHeight = 800;
 var total = 130;
 var bagPoints = [];
 var page = 'home';
+var bagInfo;
+var brands = ['Celine', 'Chanel', 'Dior', 'Fendi', 'Gucci', 'LV', 'Loewe', 'YSL'];
+var bagNames = {};
+var bagImgs = {};
 
-var pages = ['Brand', 'Style', 'Skin', 'Volume', 'Price'];
+
+//var pages = ['Brand', 'Style', 'Skin', 'Volume', 'Price'];
+var pages = ['Brand','Style', 'Price'];
 var menuPageNum = pages.length;
-var menuPageTextSize = [12,12,12,12,12];
+var menuPageTextSize = [12,12,12];
 
 var title = 'Stylish Style & Pricy Price';
 
 
 
+// BagPoint
+var dotMouseRadius = 10;
+var bagPointColor = 0;
+var bagNameTextColor = 0;
+var bagNameSize = 18;
+var bagBrandSize = 14;
+var bagNameColor = 'black';
+var bagNameX;
+var bagNameY;
+var bagImgLeftBottomX;
+var bagImgLeftBottomY;
+//var bagInfoImgWidth;
+//var bagSourceImgWidth = 1100;
+//var bagSourceImgHeight = 1100;
+
 
 
 function preload(){
-  bagInfo = loadTable("data/luxurybag_raw_data.csv", "csv", 'header');
+  bagInfo = loadTable("data/luxurybag_final_data.csv", "csv", 'header');
   
 }
 
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  bagInfoImgX = 0.75 * width;
+  bagInfoImgY = 0.05 * height;
+  bagInfoImgWidth = 0.25 * width;
+  bagNameX = 0.75 * width;
+  bagNameY = 0.5 * height;
+  bagImgLeftBottomX = 0.75 * width;
+  bagImgLeftBottomY = 0.4 * height;
+  frameRate(30);
+
+  
+  for (i = 0; i < total; i++){
+    let brand = bagInfo.getString(i, 0);
+    let name = bagInfo.getString(i, 1);
+    let style = bagInfo.getString(i, 2);
+    let skinType = bagInfo.getString(i, 4);
+    let bagColor = bagInfo.getString(i, 5);
+    let volume = bagInfo.get(i, 9);
+    let price = bagInfo.get(i, 10);
+    let x_start = map(i, 0, total, 10, width - 10);
+    let y_start = 10;
+    bagPoints.push(new BagPoint(i, x_start, y_start, brand, name, style, price, bagColor, skinType, volume));
+    if(!(brand in bagNames)){
+      bagNames[brand] = [];
+    }
+    bagNames[brand].push(name);
+  }
+  var imgPath;
+  for(var brand in bagNames){
+    for(i = 0; i < bagNames[brand].length; i++){
+    
+          name_str = bagNames[brand][i].replace(/ /g, '_');
+       if (brand === 'Louis Vitton'){
+        imgPath = 'img/'.concat('LV', '/', name_str, '.png');
+      }
+        else if(brand === 'Saint Laurent'){
+        imgPath = 'img/'.concat('YSL', '/', name_str, '.png');
+      }
+        else{
+        imgPath = 'img/'.concat(brand, '/', name_str, '.png');
+      }
+
+        bagImgs[bagNames[brand][i]] = loadImage(imgPath);
+      }
+  }
+  //console.log(bagImgs);
+  
+}
+
+
+
+
 function drawMenu(){
   var menuInnerCircleRadius = 0.2 * width;
-  var menuInnerCircleOriginX = 0.5 * width;
+  var menuInnerCircleOriginX = width;
   var menuInnerCircleOriginY = height;
   var menuRingRadius = 0.25 * width;
-  var menuRingOriginX = 0.5 * width;
+  var menuRingOriginX = width;
   var menuRingOriginY = height;
   var menuRingWidth = 3;
   var menuPageCircleRadius = 0.04 * width;
 
 
-  var menuStartAngle = PI * 7 / 6;
-  var menuEndAngle = PI * 11 / 6;
+  var menuStartAngle = PI * (1 + 1/18);
+  var menuEndAngle = PI *(3/2 - 1/18);
   var menuPageNum = pages.length;
   var menuPageDegrees = [];
   var menuPageXs = [];
@@ -46,13 +120,13 @@ function drawMenu(){
   var menuInnerCircleColor = 'gray';
   var menuPageCircleColor = 'gray';
   
-  var menuMainTextX = 0.5 * width - 0.4 * menuInnerCircleRadius;
-  var menuMainTextY = height -  0.8 * menuInnerCircleRadius;
+  var menuMainTextX = width - 0.8 * menuInnerCircleRadius;
+  var menuMainTextY = height -  0.5 * menuInnerCircleRadius;
 
   
   for (i = 0; i < pages.length; i++){
   let curretDegree = map(i, 0, menuPageNum - 1, menuStartAngle, menuEndAngle);
-  console.log(degrees(curretDegree));
+  //console.log(degrees(curretDegree));
   menuPageDegrees.push(curretDegree);
   menuPageXs.push(cos(curretDegree) * menuRingRadius + menuRingOriginX);
   menuPageYs.push(sin(curretDegree) * menuRingRadius + menuRingOriginY);
@@ -78,7 +152,7 @@ function drawMenu(){
   text(title, menuMainTextX, menuMainTextY);
   
   //Draw page circles
-  console.log(menuPageXs, menuPageYs);
+  //console.log(menuPageXs, menuPageYs);
   //circle(menuPageXs[0], menuPageYs[0], 2 * menuPageCircleRadius);
   noStroke();
   for(j = 0; j < menuPageNum;j++){
@@ -94,44 +168,136 @@ function drawMenu(){
   }
   
   pop();
+  
+
 }
 
-function setup() {
-  createCanvas(canvasWidth, canvasHeight);
+
+function drawPrice(){
+  var AxisColor = 'black';
+  var AxisOriginX = parseInt(0.1 * width);
+  //console.log(AxisOriginX);
+  var AxisOriginY =  parseInt(0.8 * height);
+  var priceAxisEndX =  parseInt(0.65 * width);
+  var priceAxisEndY = AxisOriginY;
+  var volumnAxisEndX = AxisOriginX;
+  var volumnAxisEndY =  parseInt(0.2 * height);
+  var ticksLength = 3;
   
-  // BagPoint
-  var dotMouseRadius = 10;
-  var bagInfoImgX = 0.3 * width;
-  var bagInfoImgY = 0.7 * height;
-  var bagInfoImgWidth = 0.2 * width;
-  var bagInfoImgHeight = height - bagInfoImgY;
+  var maxPriceTickX =  parseInt(0.63 * width);
+  var maxVolumnTickY =  parseInt(0.22 * height);
   
-  var bagPointColor = 0;
+  var volumnTicks = [250, 500, 750, 1000];
+  var priceTicks = [2000, 4000, 6000, 8000];
+  var priceAxisMax = 9000;
+  var volumnAxisMax = 1100;
   
-  var bagNameX = 0.5 * width;
-  var bagNameY = 0.2 * height;
+  var ticksTextSize = 16;
+  var volumnTicksTextLength = 15;
+  var priceTicksTextLength = 50;
+  var priceTicksTextYOffset = 20;
   
-  var bagNameTextColor = 0;
-  var bagNameSize = 12;
+  var volumnTicksY;
+  var priceTicksX;
+  
+  push();
+  strokeWeight(3);
+  stroke(AxisColor);
+  line(AxisOriginX, AxisOriginY, volumnAxisEndX, volumnAxisEndY);
+  line(AxisOriginX, AxisOriginY, priceAxisEndX, priceAxisEndY);
+  pop();
+  
+  for (var i = 0; i < volumnTicks.length; i++){
+    push();
+    strokeWeight(3);
+    stroke(AxisColor);
+    volumnTicksY = map(volumnTicks[i], 0, volumnAxisMax, AxisOriginY, maxVolumnTickY);
+    line(AxisOriginX, volumnTicksY, AxisOriginX + ticksLength, volumnTicksY);
+    pop();
+    push();
+    noStroke();
+    textAlign(RIGHT);
+    fill(AxisColor);
+    textSize(ticksTextSize);
+    text(volumnTicks[i], AxisOriginX - volumnTicksTextLength, volumnTicksY + 5);
+    pop();
     
-  
-  for (i = 0; i < total; i++){
-    let brand = bagInfo.getString(i, 0);
-    let name = bagInfo.getString(i, 1);
-    let style = bagInfo.getString(i, 2);
-    let skinType = bagInfo.getString(i, 4);
-    let bagColor = bagInfo.getString(i, 5);
-    let volume = bagInfo.get(i, 9);
-    let price = bagInfo.get(i, 10);
-    let x_start = map(i, 10, total, 0, width - 10);
-    let y_start = 10;
-    bagPoints.push(new BagPoint(i, x_start, y_start, brand, name, style, price, bagColor, skinType, volume));
   }
+  for (var j = 0; j < priceTicks.length; j++){
+    priceTicksX = map(priceTicks[j], 0, priceAxisMax, AxisOriginX, maxPriceTickX);
+    push();
+    strokeWeight(3);
+    stroke(AxisColor);
+    line(priceTicksX, AxisOriginY, priceTicksX, AxisOriginY - ticksLength);
+    pop();
+    push();
+    noStroke();
+    textAlign(LEFT);
+    fill(AxisColor);
+    textSize(ticksTextSize);
+    text("$" + priceTicks[j], priceTicksX - priceTicksTextLength / 2, AxisOriginY + priceTicksTextYOffset);
+    pop();
+  }
+
+  
+  push();
+  textAlign(RIGHT);
+  fill(AxisColor);
+  textSize(ticksTextSize);
+  //console.log(AxisOriginX - volumnTicksTextLength, volumnAxisEndY + 5);
+  //text('Vol /inch3', AxisOriginX - volumnTicksTextLength, volumnAxisEndY + 5);
+  text('Vol /inch3', 110, 165);
+  //textAlign(LEFT);
+  //text('Price', priceAxisEndX, AxisOriginY + priceTicksTextYOffset);
+  pop();
+  
+  
+  bagPoints.forEach(function(point){
+    var newX = map(point.getPrice(), 0, priceAxisMax, AxisOriginX, priceAxisEndX);
+    var newY = map(point.getVolumn(), 0, volumnAxisMax, AxisOriginY, volumnAxisEndY);
+    point.setX(newX);
+    point.setY(newY);
+  });
+
 }
 
+//function mouseClicked(){
+//  drawPrice();
+//  //redraw();
+//  //loop();
+//}
 
-function draw() {
+
+
+function draw(){
+  background('white');
+  //for(var i = 0; i < total; i++){
+  //  bagPoints[i].displayDot();
+  //}
+  
   drawMenu();
-  noLoop();
-
+  drawPrice();
+  //image(bagImgs[name], bagInfoImgX, bagInfoImgY, bagInfoImgWidth, bagInfoImgWidth);  
+  let nearestMouseDistance = 1000;
+  let nearestIndex; 
+  bagPoints.forEach(function(point){
+    point.updatePosition();
+    point.setNearest(false);
+    if (point.getDistance() < nearestMouseDistance){
+      nearestMouseDistance = point.getDistance();
+      nearestIndex = point.getIndex();
+    }
+  });
+    
+    
+    
+    
+  bagPoints.forEach(function(point){
+    //point.updatePosition();
+    if (point.getIndex() == nearestIndex){
+      point.setNearest(true);
+    }
+    point.display();
+  });
+  //noLoop();
 }
